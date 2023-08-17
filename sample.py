6c49f74301165bec4fd5f010ea5c1516e33c9cc7
Dion Lecaj <ledi@aeis.ch>
2023-08-17 11:27:34 +0200

    commit message

---
+++ sample.py
@@ -0,0 +1,11 @@
+print("Ich bin ein Hopper :)!")
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+app.run(host='0.0.0.0', port=8080)
